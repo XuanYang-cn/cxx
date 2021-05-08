e8eab67f8f2d73e656282b71941d047c338df42f
yangxuan <xuan.yang@zilliz.com>
2021-05-08 16:28:05 +0800

    no.146

---
+++ cxx/no.146.strings.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int
+main(){
+    std::string in_s;
+    std::cout << "Input: ";
+    std::cin >> in_s;
+    std::cout << "Result: " << in_s << std::endl;
+       
+    return 0;
+}
