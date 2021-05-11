925f4d5167865f1e1b5af3d6aeb9b0010f83ffb3
yangxuan <xuan.yang@zilliz.com>
2021-05-11 20:37:42 +0800

    add

--- cxx/no.146.strings.cpp
+++ cxx/no.146.strings.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ostream>
 #include <string>
 
 int
@@ -7,6 +8,16 @@ main(){
     std::cout << "Input: ";
     std::cin >> in_s;
     std::cout << "Result: " << in_s << std::endl;
-       
+
+    for(std::string::iterator it=in_s.begin(); it != in_s.end(); ++it) {
+        char after;
+        if ((*it) == 'z' || (*it) == 'Z'){
+            after = static_cast<char>((*it)-25);
+        } else{
+            after = static_cast<char>((*it)+1);
+        }
+        std::cout << after << std::endl;
+        
+    }
     return 0;
 }
