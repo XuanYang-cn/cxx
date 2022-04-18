1ffc9d4e7cb47efd8d8d05d2f933206e48e27187
Yang Xuan <jumpthepig@gmail.com>
2022-04-18 18:45:47 +0800

    Make compiler work on MacOS
    Signed-off-by: Yang Xuan <jumpthepig@gmail.com>

---
+++ cpp/build.sh
@@ -0,0 +1,21 @@
+CPP_SRC_DIR=$(pwd)
+BUILD_DIR="${CPP_SRC_DIR}/cmake_build"
+
+echo "CPP source dir: ${CPP_SRC_DIR}"
+echo "Build dir: ${BUILD_DIR}"
+
+if [[ ! -d ${BUILD_DIR} ]]; then
+    mkdir ${BUILD_DIR}
+    echo "Makeing build dir: ${BUILD_DIR}"
+fi
+
+cd ${BUILD_DIR} && pwd
+
+CMAKE_CMD="cmake \
+${CPP_SRC_DIR}
+"
+
+echo ${CMAKE_CMD}
+${CMAKE_CMD}
+
+make -j4
